ab82996a5529dd9e1fd6cb83fa6d89ffbc9c9b72
Peter Harris <git@peter.is-a-geek.org>
2010-09-20 16:53:51 -0400

    Add 'note' command

---
+++ Qrs/Plugin/note.pm
@@ -0,0 +1,57 @@
+package Qrs::Plugin::note;
+use Moose;
+
+use 5.10.0;
+
+use strict;
+use warnings;
+
+use Storable qw/nstore retrieve/;
+
+has 'qrs' => ( is => 'ro', isa => 'Qrs', required => 1 );
+
+has 'name' => ( is => 'ro', isa => 'Str', default => 'note', init_arg => undef );
+
+sub do {
+   my $self = shift;
+   my %arg = @_;
+
+   my $buf;
+
+   my $body = $arg{body};
+   my @cmd;
+
+   my $notes = {};
+   my $store = $self->qrs->store_file($arg{message}->from(), 'note');
+   $notes = retrieve($store) // {} if (-r $store);
+
+   my $reply = $arg{message}->make_reply();
+
+   if ($body =~ /^\s*$/) {
+      $reply->add_body("Known notes: ". join(' ', sort keys %$notes));
+      $reply->send();
+      return;
+   }
+
+   if ($body =~ /^\s*(\S+)\s*$/) {
+      $reply->add_body($notes->{$1} // "Unknown note: ". $1);
+      $reply->send();
+      return;
+   }
+
+   $body =~ s/^\s*(\S+)\s*//;
+   my $note = $1;
+   if ($body eq '-') {
+      delete $notes->{$note};
+      $reply->add_body("Note '$note' deleted.");
+   } else {
+      $notes->{$note} = $body;
+      $reply->add_body("Note '$note' stored.");
+   }
+
+   nstore $notes, $store;
+
+   $reply->send();
+}
+
+1;
